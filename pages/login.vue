c93a320ba00de30f2b3bf85cad70edb5480a3d8d
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2024-05-13 12:57:13 -0300

    add login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,15 +1,104 @@
 <template>
-  <div class="manage">
-    <h1>
-      Login page
-    </h1>
+  <div class="login flex flex-col align-items-center justify-content-center">
+    <div class="header-component component m-1">
+      <h1 class="font-bold text-3xl text-center p-3">
+        Login
+      </h1>
+    </div>
+    <div class="email-component component m-1 p-3">
+      <h2 class="font-bold text-lg mt-1 mb-3">
+        E-mail:
+      </h2>
+      <input id="email" v-model="emailInput" type="text" placeholder="Digite seu e-mail" class="w-full border-2 p-2"
+        required>
+    </div>
+    <div class="password-component component m-1 p-3">
+      <h2 class="font-bold text-lg mt-1 mb-3">
+        Senha:
+      </h2>
+      <input id="password" v-model="passwordInput" type="text" placeholder="Digite sua senha"
+        class="w-full border-2 p-2" required>
+    </div>
+    <div class="button-component component flex justify-center">
+      <NuxtLink to="/" class="flex justify-center items-center">
+        <Button severity="secondary" icon="pi pi-arrow-up-right" label="Fazer login" class="btn-profile m-1" rounded>
+          <span class="font-bold flex justify-center items-center">
+            Entrar
+            <i class="icon pi pi-arrow-up-right ml-2" style="font-size: 1rem" />
+          </span>
+        </Button>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+const emailInput = ref('')
+const passwordInput = ref('')
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.login {
+  margin: 40px auto 30px auto;
+  background-color: #000000;
+  border-radius: 20px;
+  height: 400px;
+  width: 35%;
+}
+
+.component {
+  width: 80%;
+  margin: 5px auto 5px auto;
+}
+
+h1,
+h2 {
+  color: white;
+}
+
+input {
+  border-radius: 30px;
+}
+
+input::placeholder {
+  padding-left: 5px;
+  color: #999;
+  font-style: italic;
+  opacity: 1;
+}
+
+.btn-profile {
+  background-color: black;
+  border: solid 3px;
+  color: white;
+}
+
+.btn-profile:hover {
+  background-color: white;
+  border: solid 3px;
+  color: black;
+}
+
+.btn-profile:hover .icon {
+  transition: transform 0.25s ease-in-out;
+  transform: rotate(45deg);
+}
+
+.btn-link {
+  width: auto;
+  height: auto;
+  text-decoration: none;
+}
+
+@media (max-width: 1000px) {
+  .login {
+    width: 50%;
+  }
+}
+
+@media (max-width: 400px) {
+  .login {
+    width: 80%;
+  }
+}
+</style>
